9d100f38bb961ae599730a58b6b70367671af303
Maria del Mar <chocoinformatics2016@gmail.com>
2023-03-17 12:03:17 +0100

    correct errors in the code

--- js-variables-and-numbers_social-media-post/js/index.js
+++ js-variables-and-numbers_social-media-post/js/index.js
@@ -11,16 +11,16 @@ console.clear();
 
 // --v-- write your code here --v--
 
-let title = 'How to teach without being a dictator?'
+const title = "How to teach without being a dictator?";
 
-let text = 'Attention educators! Did you know that you can teach effectively without being a dictator? The traditional authoritarian approach to teaching is no longer the only way to impart knowledge and skills to your students. By incorporating more collaborative and student-centered techniques into your classroom, you can create a more engaging and inclusive learning environment for everyone.'
+const text =
+  "Attention educators! Did you know that you can teach effectively without being a dictator? The traditional authoritarian approach to teaching is no longer the only way to impart knowledge and skills to your students. By incorporating more collaborative and student-centered techniques into your classroom, you can create a more engaging and inclusive learning environment for everyone.";
 
 let numberOfLikes = 0;
 
-let userWhoCreatedPost = 'Mar'
+const userWhoCreatedPost = "Mar";
 
 let isReported = false;
-
 
 // --^-- write your code here --^--
 
@@ -28,18 +28,17 @@ let isReported = false;
 2. Log all variables to the console. Increase the likes by one and log the amount of likes again. Adapt your code from 1.1 if necessary.
 */
 
-
 // --v-- write your code here --v--
 
-console.log( title);
-console.log( text);
-console.log('Likes:', numberOfLikes);
-console.log('Author:', userWhoCreatedPost);
-console.log('Reported;', isReported);
+console.log(title);
+console.log(text);
+console.log("Likes:", numberOfLikes);
+console.log("Author:", userWhoCreatedPost);
+console.log("Reported;", isReported);
 
-console.log('--------------------------------');
+console.log("--------------------------------");
 numberOfLikes += 1;
 
-console ('Likes': numberOfLikes);
+console.log("Likes", numberOfLikes);
 
 // --^-- write your code here --^--
