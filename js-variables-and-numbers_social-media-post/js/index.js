7973d88438639b3b0ea08a99e6f50203f6f11c9e
Maria del Mar <chocoinformatics2016@gmail.com>
2023-03-17 11:54:28 +0100

    finish challenge variables 1

---
+++ js-variables-and-numbers_social-media-post/js/index.js
@@ -0,0 +1,45 @@
+console.clear();
+
+/*
+1. Create the data for a small social media post. Create a const variable for each data point listed below.:
+	- a title
+	- a text
+	- the number of likes
+	- the user who created the post
+	- a boolean called isReported
+*/
+
+// --v-- write your code here --v--
+
+let title = 'How to teach without being a dictator?'
+
+let text = 'Attention educators! Did you know that you can teach effectively without being a dictator? The traditional authoritarian approach to teaching is no longer the only way to impart knowledge and skills to your students. By incorporating more collaborative and student-centered techniques into your classroom, you can create a more engaging and inclusive learning environment for everyone.'
+
+let numberOfLikes = 0;
+
+let userWhoCreatedPost = 'Mar'
+
+let isReported = false;
+
+
+// --^-- write your code here --^--
+
+/*
+2. Log all variables to the console. Increase the likes by one and log the amount of likes again. Adapt your code from 1.1 if necessary.
+*/
+
+
+// --v-- write your code here --v--
+
+console.log( title);
+console.log( text);
+console.log('Likes:', numberOfLikes);
+console.log('Author:', userWhoCreatedPost);
+console.log('Reported;', isReported);
+
+console.log('--------------------------------');
+numberOfLikes += 1;
+
+console ('Likes': numberOfLikes);
+
+// --^-- write your code here --^--
