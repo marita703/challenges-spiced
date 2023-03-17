33f644b08349d6b9307155a019dbb157ce888b7d
Maria del Mar <chocoinformatics2016@gmail.com>
2023-03-17 16:06:07 +0100

    finished challenges

--- js-basics_dark-mode/js/index.js
+++ js-basics_dark-mode/js/index.js
@@ -1,3 +1,23 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+const darkButton = document.querySelector('[data-js="dark-mode-button"]');
+const lightButton = document.querySelector('[data-js="light-mode-button"]');
+const togleButton = document.querySelector('[data-js="toggle-button"]');
+
+console.log();
+
+darkButton.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+  console.log("dark-mode-activated");
+});
+
+lightButton.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+  console.log("light-mode-activated");
+});
+
+togleButton.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+  console.log("togle-mode-selected");
+});
