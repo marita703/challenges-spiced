110ec5c9a77289c7475035641cc71c4e5aef7de9
Maria del Mar <chocoinformatics2016@gmail.com>
2023-03-21 15:48:28 +0100

    finish with dom exercise

--- js-functions_dom-manipulation/js/index.js
+++ js-functions_dom-manipulation/js/index.js
@@ -12,19 +12,31 @@ We want to add some functionality to the buttons on this page. When pressing a b
 */
 
 redButton.addEventListener("click", () => {
-  // Code here
+  removeAllColors();
+  addColor("red");
 });
 
 blueButton.addEventListener("click", () => {
-  // Code here
+  removeAllColors();
+  addColor("blue");
 });
 
 greenButton.addEventListener("click", () => {
-  // Code here
+  removeAllColors();
+  addColor("green");
 });
 
 grayButton.addEventListener("click", () => {
-  // Code here
+  removeAllColors();
+  addColor("gray");
 });
 
 // Write your two functions below:
+
+function removeAllColors() {
+  box.classList.remove("gray", "green", "blue", "red");
+}
+
+function addColor(color) {
+  box.classList.add(color);
+}
