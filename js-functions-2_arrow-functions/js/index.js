9440b049e694865d0142bdec8b6fd45ef31c7e9d
Maria del Mar <chocoinformatics2016@gmail.com>
2023-03-22 13:50:49 +0100

    finish arrow excersise

--- js-functions-2_arrow-functions/js/index.js
+++ js-functions-2_arrow-functions/js/index.js
@@ -5,7 +5,22 @@ Rewrite the following functions as arrow functions.
 Use implicit returns when possible
 */
 
-function getCurrentHour() {
+//ORIGINAL FUNCTION
+// function getCurrentHour() {
+//   const now = new Date();
+//   const currentHour = now.getHours();
+//   if (currentHour === 0) {
+//     return "12am";
+//   } else if (currentHour === 12) {
+//     return "12pm";
+//   } else if (currentHour <= 11) {
+//     return currentHour + "am";
+//   } else {
+//     return currentHour - 12 + "pm";
+//   }
+// }
+
+const getCurrentHour = () => {
   const now = new Date();
   const currentHour = now.getHours();
   if (currentHour === 0) {
@@ -14,35 +29,61 @@ function getCurrentHour() {
     return "12pm";
   } else if (currentHour <= 11) {
     return currentHour + "am";
-  } else {
-    return currentHour - 12 + "pm";
   }
-}
+  return currentHour - 12 + "pm";
+};
 
-function getVectorLength(x, y, z) {
-  return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
-}
+//ORIGINAL FUNCTION
+// function getVectorLength(x, y, z) {
+//   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
+// }
 
-function cleanInput(string) {
-  return string.toLowerCase().trim();
-}
+const getVectorLength = (x, y, z) => (x ** 2 + y ** 2 + z ** 2) ** 0.5;
+
+// Original FUNCTION
+// function cleanInput(string) {
+//   return string.toLowerCase().trim();
+// }
+
+const cleanInput = (string) => string.toLowerCase().trim();
 
 /*
 Rewrite the following arrow functions as classic functions.
 */
 
-const isOddAndSmall = (number) => {
+//Original Function
+//  const isOddAndSmall = (number) => {
+//   if (number > 10) {
+//     return false;
+//   }
+
+//   if (number % 2 === 0) {
+//     return false;
+//   }
+
+//   return true;
+// };
+
+function isOddAndSmall(number) {
   if (number > 10) {
     return false;
-  }
-
-  if (number % 2 === 0) {
+  } else if (number % 2 === 0) {
     return false;
+  } else {
+    return true;
   }
+}
 
-  return true;
-};
+//Original function
+// const add3 = (a, b, c) => a + b + c;
 
-const add3 = (a, b, c) => a + b + c;
+function add3(a, b, c) {
+  return a + b + c;
+}
 
-const repeat10 = (string) => string.repeat(10);
+//Original Function
+// const repeat10 = (string) => string.repeat(10);
+
+function repeat10(string) {
+  return string.repeat(10);
+}
