298942993a3d4d5d9183f73a79145f1c0d8241ec
Maria del Mar <chocoinformatics2016@gmail.com>
2023-03-23 17:05:34 +0100

    finished challenge 3

--- js-forms_calculator/js/index.js
+++ js-forms_calculator/js/index.js
@@ -22,9 +22,29 @@ function divide(a, b) {
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  const a = parseInt(form.numberA.value);
+  const b = parseInt(form.numberB.value);
+  const sum = form.addition.checked;
+  const subs = form.subtract.checked;
+  const mult = form.multiplication.checked;
+  const div = form.division.checked;
+  console.log(sum, mult, div, subs);
+
+  console.log(a, b);
   let result;
 
   // --v-- write your code here --v--
+
+  if (sum === true) {
+    result = add(a, b);
+    console.log(add(a, b));
+  } else if (subs === true) {
+    result = substract(a, b);
+  } else if (mult === true) {
+    result = multiply(a, b);
+  } else {
+    result = divide(a, b);
+  }
 
   // --^-- write your code here --^--
 
