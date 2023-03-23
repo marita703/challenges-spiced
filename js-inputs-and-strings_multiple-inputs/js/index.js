b8598b7d11abc63a441983e55aa8ae8bf43f285d
Maria del Mar <chocoinformatics2016@gmail.com>
2023-03-23 13:53:22 +0100

    finish third excersice

--- js-inputs-and-strings_multiple-inputs/js/index.js
+++ js-inputs-and-strings_multiple-inputs/js/index.js
@@ -14,3 +14,12 @@ Implement the following functionality:
 const firstInput = document.querySelector("[data-js=first-input]");
 const secondInput = document.querySelector("[data-js=second-input]");
 const button = document.querySelector("[data-js=button]");
+
+button.addEventListener("click", () => {
+  let first = firstInput.value;
+  let second = secondInput.value;
+  let firstUpper = first.toUpperCase();
+
+  firstInput.value = second;
+  secondInput.value = firstUpper;
+});
