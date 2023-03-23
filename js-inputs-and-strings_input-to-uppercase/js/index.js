31c1c9690fa8b2ec6828cbccd2c23dc06c5ae52a
Maria del Mar <chocoinformatics2016@gmail.com>
2023-03-23 11:47:08 +0100

    first challenge done

--- js-inputs-and-strings_input-to-uppercase/js/index.js
+++ js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -10,3 +10,15 @@ Convert the input value to uppercase when clicking the button
 Hint: Access the value by using `.value` on the input element
 
 */
+
+let button = document.querySelector('[data-js="button-uppercase"]');
+
+let textBox = document.querySelector('[data-js="first-input"]');
+
+button.addEventListener("click", () => {
+  const message = textBox.value;
+
+  console.log(message);
+
+  textBox.value = message.toUpperCase();
+});
