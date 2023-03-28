031eaa30c8e2e24bac6c0f6c95c224803eeaa8df
Maria del Mar <chocoinformatics2016@gmail.com>
2023-03-28 15:25:04 +0200

    finished with list loopchallenge

---
+++ js-loops_forof-list/js/index.js
@@ -0,0 +1,33 @@
+console.clear();
+
+const main = document.querySelector('[data-js="main"]');
+
+// Create a list from an array
+
+const ol = document.createElement("ol");
+main.append(ol);
+
+const programmingLanguages = [
+  "JavaScript",
+  "Python",
+  "Java",
+  "C#",
+  "C++",
+  "PHP",
+  "Ruby",
+];
+
+// --v-- write/change code here --v--
+
+for (const language of programmingLanguages) {
+  const li = document.createElement("li");
+  li.textContent = language;
+  ol.append(li);
+  console.log(language);
+}
+
+for (let i = 0; i < programmingLanguages.length; i++) {
+  console.log(programmingLanguages[i]);
+}
+
+// --^-- write/change code here --^--
