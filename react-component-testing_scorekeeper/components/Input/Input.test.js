1fd197215299a3dda6cd4780ed2509201cbcec75
Maria del Mar <chocoinformatics2016@gmail.com>
2023-04-24 15:02:52 +0200

    finish testing with the input component

--- react-component-testing_scorekeeper/components/Input/Input.test.js
+++ react-component-testing_scorekeeper/components/Input/Input.test.js
@@ -2,6 +2,43 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Input from ".";
 
-test("renders a label and an input with the correct attributes", () => {});
+test("renders a label and an input with the correct attributes", () => {
+  const handleCheck = jest.fn();
 
-test("calls callback on every user input", async () => {});
+  render(
+    <Input
+      labelText="labelText"
+      placeholder="placeHolder"
+      name="name"
+      value="value"
+      onChange={handleCheck()}
+      required
+    />
+  );
+  const label = screen.getByText("labelText");
+  expect(label).toBeInTheDocument();
+
+  const input = screen.getByLabelText("labelText");
+  expect(input).toBeInTheDocument();
+});
+
+test("calls callback on every user input", async () => {
+  const handleCheck = jest.fn();
+  const user = userEvent.setup();
+
+  render(
+    <Input
+      labelText="labelText"
+      placeholder="placeHolder"
+      name="name"
+      value="value"
+      onChange={handleCheck()}
+      required
+    />
+  );
+
+  const input = screen.getByLabelText("labelText");
+  expect(input).toBeInTheDocument();
+
+  await user.type(input, "Game of Thrones");
+});
